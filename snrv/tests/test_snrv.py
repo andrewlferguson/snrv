7fcc8c4229d0e58be25347d64922961f27176f19
Andrew L Ferguson <andrew.l.ferguson@gmail.com>
2022-01-18 12:49:58 -0600

    Adding pytest tests

--- snrv/tests/test_snrv.py
+++ snrv/tests/test_snrv.py
@@ -10,14 +10,20 @@ from scipy.linalg import eigh
 import pytest
 import snrv
 import torch
+import copy
 
 
 def test_snrv_imported():
-    """Sample test, will always pass so long as import statement worked."""
+    """
+    Testing import successful.
+    """
     assert "snrv" in sys.modules
 
 
 def test__stable_symmetric_inverse():
+    """
+    Testing inversion of symmetric matrix routine.
+    """
     A = np.array([[2.33579236, 0.41457851, 1.0944027],
                   [0.41457851, 1.52068338, 0.36892426],
                   [1.0944027, 0.36892426, 1.88039527]])
@@ -33,6 +39,9 @@ def test__stable_symmetric_inverse():
 
 
 def test__stable_symmetric_inverse_sqrt():
+    """
+    Testing inversion of symmetric matrix routine with sqrt return.
+    """
     A = np.array([[2.33579236, 0.41457851, 1.0944027],
                   [0.41457851, 1.52068338, 0.36892426],
                   [1.0944027, 0.36892426, 1.88039527]])
@@ -48,13 +57,16 @@ def test__stable_symmetric_inverse_sqrt():
 
 
 def test__gen_eig_chol():
+    """
+    Testing Cholesky factorization based solution to symmetric generalized eigenvalue problem.
+    """
     C = np.array([[2.33579236, 0.41457851, 1.0944027],
                   [0.41457851, 1.52068338, 0.36892426],
                   [1.0944027, 0.36892426, 1.88039527]])
 
     assert np.allclose(C, C.T)
 
-    Q = np.array([[1.4536383 , 1.24153722, 1.66784209],
+    Q = np.array([[1.4536383, 1.24153722, 1.66784209],
                   [1.24153722, 1.70993988, 1.18609631],
                   [1.66784209, 1.18609631, 2.38190968]])
 
@@ -73,6 +85,52 @@ def test__gen_eig_chol():
     assert np.allclose(w, w2) and np.allclose(v, v2)
 
 
+def test_fit():
+    """
+    Testing fit call updates model parameters.
+    """
 
+    np.random.seed(42)
+    torch.manual_seed(42)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    # synthetic training data
+    x = np.linspace(0, 10 * np.pi, 1000)
+    traj_x = np.concatenate((np.reshape(np.sin(x), (-1, 1)), np.reshape(np.cos(x), (-1, 1))), 1)
+    traj_x_featurized = traj_x
+    traj_x_featurized = torch.from_numpy(traj_x_featurized).float()
 
+    # building model
+    input_size = traj_x_featurized.size()[1]
+    output_size = 4
+    hidden_depth = 2
+    hidden_size = 100
+    batch_norm = True
+    dropout_rate = 0.
+    lr = 1E-2
+    weight_decay = 0.
+    val_frac = 0.10
+    n_epochs = 10
+    batch_size = 100
+    VAMPdegree = 2
+    is_reversible = False
+
+    model = snrv.Snrv(input_size, output_size, hidden_depth=hidden_depth, hidden_size=hidden_size,
+                      batch_norm=batch_norm, dropout_rate=dropout_rate, lr=lr, weight_decay=weight_decay,
+                      val_frac=val_frac, n_epochs=n_epochs, batch_size=batch_size,
+                      VAMPdegree=VAMPdegree, is_reversible=is_reversible)
+    model = model.to(device)
+
+    # initial parameters
+    params_before = copy.deepcopy(list(model.parameters()))
+
+    # fitting model
+    lag_n = 10
+    model.fit(traj_x_featurized, lag_n)
+
+    # final parameters
+    params_after = copy.deepcopy(list(model.parameters()))
+
+    # checking (at least one) parameter in every parameter block has been updated
+    for ii in range(len(params_before)):
+        assert (params_before[ii] != params_after[ii]).any()
