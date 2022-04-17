48585915c9bc41af908fddb00bee9dd2febaffd1
KirillShmilovich <kirills@midway2-login2.rcc.local>
2022-04-16 22:39:16 -0500

    muli-gpu seed fix

--- snrv/utils.py
+++ snrv/utils.py
@@ -209,4 +209,4 @@ def set_random_seed(seed=42):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed(seed)
